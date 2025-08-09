f4b8c44650f276c4f5875d1d73cae29035868745
Hà inKi <inki.personal@gmail.com>
2025-08-10 02:13:18 +0700

    feat: use --bun and --turbopack to optimize development

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from 'next'
 import createNextIntlPlugin from 'next-intl/plugin'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  turbopack: {
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
 
 const withNextIntl = createNextIntlPlugin(
   './src/infrastructure/i18n/request.ts',
